2ef711776ea314b954c114688d04113c26d4a6f2
thuyhatruong1708-rgb <thuyhatruong1708@gmail.com>
2025-12-03 00:56:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,32 +4,39 @@ function recommend() {
 
     if (input === "beach") {
         results.innerHTML = `
-        <h2>Beach Recommendations</h2>
-        <img src="images/beach1.jpg">
-        <img src="images/beach2.jpg">
-        <p>Relax on the world's most beautiful beaches.</p>
+            <h2>Beach Recommendations</h2>
+            <div class="img-row">
+                <img src="images/beach1.avif" alt="Cambodia beach">
+                <img src="images/beach2.jpg" alt="Qinghai">
+            </div>
+            <p>Relax on the world's most beautiful beaches.</p>
         `;
     }
 
     else if (input === "temple") {
         results.innerHTML = `
-        <h2>Temple Recommendations</h2>
-        <img src="images/temple1.jpg">
-        <img src="images/temple2.jpg">
-        <p>Explore stunning cultural and religious temple sites.</p>
+            <h2>Temple Recommendations</h2>
+            <div class="img-row">
+                <img src="images/temple1.jfif" alt="Angkor Wat">
+                <img src="images/temple2.jfif" alt="Chinese Temple">
+            </div>
+            <p>Explore stunning cultural and religious temple sites.</p>
         `;
     }
 
     else if (input === "country") {
         results.innerHTML = `
-        <h2>Country Recommendations</h2>
-        <img src="images/country1.jpg">
-        <img src="images/country2.jpg">
-        <p>Discover top sightseeing destinations from countries around the world.</p>
+            <h2>Country Recommendations</h2>
+            <div class="img-row">
+                <img src="images/country1.jpg" alt="Cambodia">
+                <img src="images/country2.jfif" alt="China">
+            </div>
+            <p>Discover top sightseeing destinations from countries around the world.</p>
         `;
     }
 
     else {
-        results.innerHTML = `<p>No results found. Try beach, temple, or country.</p>`;
+        results.innerHTML = `<p>No results found. Try: <strong>beach</strong>, <strong>temple</strong>, or <strong>country</strong>.</p>`;
     }
 }
+
