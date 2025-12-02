2e645703f6fc8afcb547853c01210490acf50333
thuyhatruong1708-rgb <thuyhatruong1708@gmail.com>
2025-12-03 01:16:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ function recommend() {
     const input = document.getElementById("searchInput").value.toLowerCase();
     const results = document.getElementById("results");
 
-    const basePath = "images/";
+    const basePath = "https://thuyhatruong1708-rgb.github.io/travel-recommendation/images/";
 
     if (input === "beach") {
         results.innerHTML = `
             <h2>Beach Recommendations</h2>
             <img src="${basePath}beach1.avif">
             <img src="${basePath}beach2.jpg">
-            <p>Relax on the world's most beautiful beaches.</p>
         `;
     }
 
@@ -18,7 +17,6 @@ function recommend() {
             <h2>Temple Recommendations</h2>
             <img src="${basePath}temple1.jfif">
             <img src="${basePath}temple2.jfif">
-            <p>Explore cultural and religious temple sites.</p>
         `;
     }
 
@@ -27,11 +25,10 @@ function recommend() {
             <h2>Country Recommendations</h2>
             <img src="${basePath}country1.jpg">
             <img src="${basePath}country2.jfif">
-            <p>Discover beautiful destinations around the world.</p>
         `;
     }
 
     else {
-        results.innerHTML = `<p>No results found. Try typing: beach, temple, or country.</p>`;
+        results.innerHTML = `<p>No results found. Try: beach, temple, country.</p>`;
     }
 }
